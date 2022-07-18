d10749a892fa382613d3c1afdb71c19ce2305950
Cora Kingdon <ckingdon@berkeley.edu>
2022-07-18 13:26:00 -0500

    modify shell script

--- test1.sh
+++ test1.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=test1
 #SBATCH --account=fc_fusionacs
 #SBATCH --partition=savio2
-#SBATCH --time=01:00:00 #one hour
+#SBATCH --time=00:04:00 
 
 Module load r
 
-R CMD BATCH ~/repos/RECS-ACS/test1.R /global/scratch/users/ckingdon/logs/test1_log.Rout
+R CMD BATCH ~/repos/fusionACS_savio/test1.R /global/scratch/users/ckingdon/logs/test1_log.Rout
