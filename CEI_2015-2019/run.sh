54896b12f145f4588f06ec48a16c9ece764ea034
Cora Kingdon <ckingdon@berkeley.edu>
2022-07-25 14:43:47 -0700

    add schell script for job

---
+++ CEI_2015-2019/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=cei_production
+#SBATCH --account=fc_fusionacs
+#SBATCH --partition=savio2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=24
+#SBATCH --time=00:10:00 
+
+module load r
+module load r-packages
+
+R CMD BATCH ~/repos/fusionACS_savio/CEI_2015-2019/CEI_2015-2019_output_SUBSET.R /global/scratch/users/ckingdon/logs/cei_log.Rout
