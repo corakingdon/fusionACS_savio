fe776c8f47d2f21dff5bd618409ca2b096818723
Cora Kingdon <ckingdon@berkeley.edu>
2022-07-29 14:00:43 -0700

    rename 2019 and update run.sh

--- CEI_2015-2019/run.sh
+++ CEI_2015-2019/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=cei_train
+#SBATCH --job-name=cei_production
 #SBATCH --account=fc_fusionacs
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
-#SBATCH --time=00:10:00 
+#SBATCH --time=03:00:00 
 
 module load r
 module load r-packages
 
-R CMD BATCH ~/repos/fusionACS_savio/CEI_2015-2019/train.R /global/scratch/users/ckingdon/logs/cei_train_log.Rout
+R CMD BATCH ~/repos/fusionACS_savio/CEI_2015-2019/CEI_2019_output.R /global/scratch/users/ckingdon/logs/cei_log.Rout
