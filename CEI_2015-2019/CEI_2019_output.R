c4620ab24e9d4d50d5f594286d9e2f15454fa6dd
Cora Kingdon <ckingdon@berkeley.edu>
2022-08-05 12:02:10 -0700

    updates for fuse memory issue

--- CEI_2015-2019/CEI_2019_output.R
+++ CEI_2015-2019/CEI_2019_output.R
@@ -17,16 +17,17 @@ if(local.run){
 #------------------------------------------------------------------------------
 
 # Turn off data.table and fst multithreading to prevent forking issues
-setDTthreads(1)
-threads_fst(1)
+data.table::setDTthreads(1)
+fst::threads_fst(1)
 
 #------------------ Prep the inputs --------------------------------------------
 
 # Load the training data
-train.data <- read_fst(file.path(in.dir, "CEI_2015-2019_2019_train.fst"))
+train.data <- fst::read_fst(file.path(in.dir, "CEI_2015-2019_2019_train.fst"))
 
 # Extract variable names from the prediction data (without loading to memory)
-pred.vars <- names(fst(file.path(in.dir, "CEI_2015-2019_2019_predict.fst")))
+pred.vars <- names(fst::fst(file.path(in.dir, "CEI_2015-2019_2019_predict.fst")))
+
 
 # Identify the fusion variables
 fusion.vars <- setdiff(names(train.data), c("weight", pred.vars))
@@ -73,7 +74,7 @@ train.data <- train.data[fsimp, ]
 #                   weight = "weight",
 #                   nfolds = 0.75,
 #                   fork = TRUE,
-#                   cores = 12,
+#                   cores = num.cores,
 #                   hyper = list(boosting = "goss",
 #                                num_leaves = 2 ^ (5) - 1,
 #                                min_data_in_leaf = unique(round(pmax(10, length(fsimp) * 0.0005 * c(1)))),
@@ -84,8 +85,8 @@ train.data <- train.data[fsimp, ]
 # print(Sys.time() - start)
 
 # Once train() is complete, reset number of threads allowed in data.table and fst
-setDTthreads(num.cores)
-threads_fst(num.cores)
+data.table::setDTthreads(num.cores)
+fst::threads_fst(num.cores)
 
 #------------------ Fuse - validation ------------------------------------------
 
@@ -104,11 +105,13 @@ threads_fst(num.cores)
 # 
 # # Clean up
 # rm(train.data, valid)
+# gc()
 
 #------------------ Fuse - simulation ------------------------------------------
 
 # Load the prediction data
-pred.data <- read_fst(file.path(in.dir, "CEI_2015-2019_2019_predict.fst"))
+pred.data <- fst::read_fst(file.path(in.dir, "CEI_2015-2019_2019_predict.fst"))
+
 
 # Fuse multiple implicates to ACS
 fsn.path = file.path(out.dir, "CEI_2015-2019_2019_model.fsn")
@@ -117,7 +120,7 @@ sim <- fuse(data = pred.data,
             file = fsn.path,
             k = 10,
             M = 30,
-            cores = 12)
+            cores = num.cores)
 print(Sys.time() - start)
 
 # Save 'sim' as .fst
@@ -125,3 +128,4 @@ fst::write_fst(x = sim, path = file.path(out.dir, "CEI_2015-2019_2019_fused.fst"
 
 # Clean up
 rm(pred.data, sim)
+gc()
